1bae40c4fc5acf2ac096536436366b30626bfca7
rahomohamed <rahojmohamed@gmail.com>
2020-02-03 20:52:47 +0000

    add head.js

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨 🚨 🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const head = function(arr) {
+  return (arr[0]);
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
