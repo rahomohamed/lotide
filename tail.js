07c5b7bc1231b87036c2b4985479fdd07ed82615
rahomohamed <rahojmohamed@gmail.com>
2020-02-03 20:57:51 +0000

    made changes

--- tail.js
+++ tail.js
@@ -7,11 +7,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
-const head = function(arr) {
-  return (arr[0]);
-}
-
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
