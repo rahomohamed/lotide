cd3ac27c8a1c2e2642a3875969d42eaeef809d78
rahomohamed <rahojmohamed@gmail.com>
2020-02-06 21:04:15 +0000

    implemented assertarraysequal

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,28 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) { 
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) { 
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🚨 🚨 🚨 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+
 const takeUntil = function(array, callback) {
 const results = [];
  for (let item of array) {
